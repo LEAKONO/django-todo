ebfae30c97cd39646900f30c823a3bd0d75d054d
LEAKONO <leakonoemmanuel3@gmail.com>
2025-04-19 17:22:21 +0300

    feat: add basic views and URLs

--- myapp/views.py
+++ myapp/views.py
@@ -2,18 +2,18 @@
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Task
-from .serializers import TaskSerializer
+from .models import Todo
+from .serializers import TodoSerializer
 
 @api_view(['GET'])
 def get_tasks(request):
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
+    tasks = Todo.objects.all()
+    serializer = TodoSerializer(tasks, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
 def create_task(request):
-    serializer = TaskSerializer(data=request.data)
+    serializer = TodoSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,7 @@
-# todo/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('', include('myapp.urls')),  # includes routes from your app
 ]
